c7c70bcd8ab4d774b04e010637d7b55437b11e82
danielcrubens <dcr.tecnologi@gmail.com>
2022-11-06 12:19:40 -0300

    consumindo API  :white_check_mark:

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,40 +1,24 @@
+import {apiKey} from"../js/key.js"
+
 const moviesContainer = document.querySelector('.movies')
 
-const movies = [
-  {
-    image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
-    title: 'Batman',
-    rating: 9.2,
-    year: 2022,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    isFavorited: false
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
-    title: 'Avengers',
-    rating: 9.2,
-    year: 2019,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    isFavorited: false
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
-    title: 'Doctor Strange',
-    rating: 9.2,
-    year: 2022,
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    isFavorited: true
-  },
-]
+async function getPopularMovies() {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+  const fetchResponse = await fetch(url)
+  const { results } = await fetchResponse.json()
+  return results
+} 
 
-window.onload = function() {
+window.onload = async function() {
   console.log('movie')
+  const movies = await getPopularMovies()
   movies.forEach(movie => renderMovie(movie))
 }
 
 function renderMovie(movie) {
 
-  const { title, image, rating, year, description, isFavorited } = movie
+  const { title, image, rating, year, description } = movie
+  const isFavorited = false
 
   const movieElement = document.createElement('div')
   movieElement.classList.add('movie')
