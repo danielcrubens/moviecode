61dc270dff6037f91bcd7e3052f60cf4ce894bb1
danielcrubens <dcr.tecnologi@gmail.com>
2022-11-12 15:27:15 -0300

    favoritando/desfavoritando filme salvo no local storage  :white_check_mark:

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -39,11 +39,52 @@ async function getPopularMovies() {
   return results
 } 
 
+function favoriteButtonPressed(event, movie) {
+  const favoriteState = {
+    favorited: 'images/heart-fill.svg',
+    notFavorited: 'images/heart.svg'
+  }
+
+  if(event.target.src.includes(favoriteState.notFavorited)) {
+    // aqui ele será favoritado
+    event.target.src = favoriteState.favorited
+    saveToLocalStorage(movie)
+  } else {
+    // aqui ele será desfavoritado
+    event.target.src = favoriteState.notFavorited
+    removeFromLocalStorage(movie.id)
+  }
+}
+
+function getFavoriteMovies() {
+  return JSON.parse(localStorage.getItem('favoriteMovies'))
+}
+
+function saveToLocalStorage(movie) {
+  const movies = getFavoriteMovies() || []
+  movies.push(movie)
+  const moviesJSON = JSON.stringify(movies)
+  localStorage.setItem('favoriteMovies', moviesJSON)
+}
+
+function checkMovieIsFavorited(id) {
+  const movies = getFavoriteMovies() || []
+  return movies.find(movie => movie.id == id)
+}
+
+function removeFromLocalStorage(id) {
+  const movies = getFavoriteMovies() || []
+  const findMovie = movies.find(movie => movie.id == id)
+  const newMovies = movies.filter(movie => movie.id != findMovie.id)
+  localStorage.setItem('favoriteMovies', JSON.stringify(newMovies))
+}
+
 window.onload = async function() {
   console.log('movie')
   const movies = await getPopularMovies()
   movies.forEach(movie => renderMovie(movie))
 }
+
 
 function renderMovie(movie) {
 
