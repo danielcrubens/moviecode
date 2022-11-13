4205be88a6da6e25b70cd633582ceb78068860ec
danielcrubens <dcr.tecnologi@gmail.com>
2022-11-13 17:45:46 -0300

    refatorando movie  :white_check_mark:

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,24 +1,40 @@
-import {apiKey} from"../js/key.js"
+
+
+import { API } from '/services/api.js'
+import { LocalStorage } from '/services/localStorage.js'
 
 const moviesContainer = document.querySelector('.movies')
+const form = document.querySelector('form')
 const input = document.querySelector('input')
 const searchButton = document.querySelector('.searchIcon')
+const checkboxInput = document.querySelector('input[type="checkbox"]')
 
-searchButton.addEventListener('click',searchMovie)
-input.addEventListener('keyup',function(event){
-  console.log(event.key)
-  if (event.keyCode ==13){
-    searchMovie()
-    return
-  }
+checkboxInput.addEventListener('change', checkCheckboxStatus)
+searchButton.addEventListener('click', searchMovie)
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  searchMovie()
 })
 
-async function searchMovie(){
-  const inputValue = input.value
-  if(inputValue != ''){
-    cleanAllMovies()
-    const movies = await searchMovieByName(inputValue)
+function checkCheckboxStatus() {
+  const isChecked = checkboxInput.checked
+  cleanAllMovies()
+  if (isChecked) {
+    const movies = LocalStorage.getFavoriteMovies() || []
     movies.forEach(movie => renderMovie(movie))
+  } else {
+    getAllPopularMovies()
+  }
+}
+
+async function searchMovie() {
+  const inputValue = input.value
+  cleanAllMovies()
+  if (inputValue != '') {
+    const movies = await API.searchMovieByName(inputValue)
+    movies.forEach(movie => renderMovie(movie))
+  } else {
+    getAllPopularMovies()
   }
 }
 
@@ -26,70 +42,38 @@ function cleanAllMovies() {
   moviesContainer.innerHTML = ''
 }
 
-async function searchMovieByName(title) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1`
-  const fetchResponse = await fetch(url)
-  const { results } = await fetchResponse.json()
-  return results
-}
-async function getPopularMovies() {
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-  const fetchResponse = await fetch(url)
-  const { results } = await fetchResponse.json()
-  return results
-} 
-
 function favoriteButtonPressed(event, movie) {
   const favoriteState = {
-    favorited: 'images/heart-fill.svg',
-    notFavorited: 'images/heart.svg'
+    favorited: 'assets/img/heart-fill.png',
+    notFavorited: 'assets/img/heart.png'
   }
-
-  if(event.target.src.includes(favoriteState.notFavorited)) {
+  if (event.target.src.includes(favoriteState.notFavorited)) {
     // aqui ele será favoritado
     event.target.src = favoriteState.favorited
-    saveToLocalStorage(movie)
+    LocalStorage.saveToLocalStorage(movie)
   } else {
     // aqui ele será desfavoritado
     event.target.src = favoriteState.notFavorited
-    removeFromLocalStorage(movie.id)
+    LocalStorage.removeFromLocalStorage(movie.id)
   }
 }
 
-function getFavoriteMovies() {
-  return JSON.parse(localStorage.getItem('favoriteMovies'))
-}
-
-function saveToLocalStorage(movie) {
-  const movies = getFavoriteMovies() || []
-  movies.push(movie)
-  const moviesJSON = JSON.stringify(movies)
-  localStorage.setItem('favoriteMovies', moviesJSON)
-}
-
-function checkMovieIsFavorited(id) {
-  const movies = getFavoriteMovies() || []
-  return movies.find(movie => movie.id == id)
-}
-
-function removeFromLocalStorage(id) {
-  const movies = getFavoriteMovies() || []
-  const findMovie = movies.find(movie => movie.id == id)
-  const newMovies = movies.filter(movie => movie.id != findMovie.id)
-  localStorage.setItem('favoriteMovies', JSON.stringify(newMovies))
-}
-
-window.onload = async function() {
-  console.log('movie')
-  const movies = await getPopularMovies()
+async function getAllPopularMovies() {
+  const movies = await API.getPopularMovies()
   movies.forEach(movie => renderMovie(movie))
 }
 
+window.onload = function () {
+
+  getAllPopularMovies()
+}
 
 function renderMovie(movie) {
+  const { id, title, poster_path, vote_average, release_date, overview } = movie
 
-  const { title, image, rating, year, description } = movie
-  const isFavorited = false
+  const isFavorited = LocalStorage.checkMovieIsFavorited(id)
+  const year = new Date(release_date).getFullYear()
+  const image = `https://image.tmdb.org/t/p/w500${poster_path}`
 
   const movieElement = document.createElement('div')
   movieElement.classList.add('movie')
@@ -100,54 +84,82 @@ function renderMovie(movie) {
 
   const movieImageContainer = document.createElement('div')
   movieImageContainer.classList.add('movie-image')
+
   const movieImage = document.createElement('img')
   movieImage.src = image
   movieImage.alt = `${title} Poster`
+
   movieImageContainer.appendChild(movieImage)
   movieInformations.appendChild(movieImageContainer)
 
   const movieTextContainer = document.createElement('div')
   movieTextContainer.classList.add('movie-text')
+
   const movieTitle = document.createElement('h4')
   movieTitle.textContent = `${title} (${year})`
+
   movieTextContainer.appendChild(movieTitle)
   movieInformations.appendChild(movieTextContainer)
 
   const informations = document.createElement('div')
   informations.classList.add('movie-informations')
+
   movieTextContainer.appendChild(informations)
 
   const ratingContainer = document.createElement('div')
   ratingContainer.classList.add('rating')
+
   const starImage = document.createElement('img')
   starImage.src = 'assets/img/star.png'
   starImage.alt = 'Star'
+
   const movieRate = document.createElement('span')
   movieRate.classList.add('movie-rate')
-  movieRate.textContent = rating
+  movieRate.textContent = vote_average
+
   ratingContainer.appendChild(starImage)
   ratingContainer.appendChild(movieRate)
   informations.appendChild(ratingContainer)
 
   const favorite = document.createElement('div')
   favorite.classList.add('favorite')
+
   const favoriteImage = document.createElement('img')
   favoriteImage.src = isFavorited ? 'assets/img/heart-fill.png' : 'assets/img/heart.png'
   favoriteImage.alt = 'Heart'
   favoriteImage.classList.add('favoriteImage')
+  favoriteImage.addEventListener('click', (event) => favoriteButtonPressed(event, movie))
+
   const favoriteText = document.createElement('span')
   favoriteText.classList.add('movie-favorite')
   favoriteText.textContent = 'Favoritar'
+
   favorite.appendChild(favoriteImage)
   favorite.appendChild(favoriteText)
   informations.appendChild(favorite)
 
   const movieDescriptionContainer = document.createElement('div')
   movieDescriptionContainer.classList.add('movie-description')
+
   const movieDescription = document.createElement('span')
-  movieDescription.textContent = description
+  movieDescription.textContent = overview
+
   movieDescriptionContainer.appendChild(movieDescription)
-  
+
   movieElement.appendChild(movieInformations)
   movieElement.appendChild(movieDescriptionContainer)
+}
+
+const scrollTopBtn = document.querySelector('#scrollTop')
+if (scrollTopBtn) {
+  scrollTopBtn.addEventListener('click', function () {
+    window.scrollTo(0, 0,)
+  })
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset >= 900) {
+      scrollTopBtn.classList.add('active')
+    } else {
+      scrollTopBtn.classList.remove('active')
+    }
+  })
 }
---
+++ services/api.js
@@ -0,0 +1,19 @@
+import {apiKey} from"/services/key.js"
+
+async function searchMovieByName(title) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1`
+    const fetchResponse = await fetch(url)
+    const { results } = await fetchResponse.json()
+    return results
+  }
+  async function getPopularMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+    const fetchResponse = await fetch(url)
+    const { results } = await fetchResponse.json()
+    return results
+  } 
+
+export const API = {
+    searchMovieByName,
+    getPopularMovies
+  }
